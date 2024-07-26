ee7b3467ecbca87be40e667340b756d8446ebb86
Abdinasir-Osman <saacidosmaanxaaji@gmail.com>
2024-07-26 13:41:13 +0300

    imported react Icons

--- src/components/Contact us/ContactUs.jsx
+++ src/components/Contact us/ContactUs.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import { IoIosCall } from "react-icons/io";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
+import { FaSquareXTwitter, FaSquareYoutube } from "react-icons/fa6";
+import { CiLocationOn } from "react-icons/ci";
 
 export default function ContactUs() {
 
